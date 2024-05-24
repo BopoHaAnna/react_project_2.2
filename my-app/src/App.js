075fa106c8199c011e41096b7834f97dad344cea
Anna Yastrebova <direct.yastrebova@gmail.com>
2024-05-24 21:03:16 +0200

    hw 2.2

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,36 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styles from './app.module.css';
+import data from './data.json';
+import React, { useState } from 'react';
 
 export const App = () => {
-	// Императивный
-	const currentYear = new Date().getFullYear();
-	// Декларативный
-	return React.createElement(
-		'div',
-		{ className: 'App' },
-		React.createElement(
-			'header',
-			{ className: 'App-header' },
-			React.createElement('img', { src: logo, className: 'App-logo', alt: 'logo' }),
-			React.createElement(
-				'p',
-				null,
-				'Edit ',
-				React.createElement('code', null, 'src/App.js'),
-				' and save to reload.',
-			),
-			React.createElement(
-				'a',
-				{
-					className: 'App-link',
-					href: 'https://reactjs.org',
-					target: '_blank',
-					rel: 'noopener noreferrer',
-				},
-				'Learn React',
-			),
-			React.createElement('p', null, currentYear),
-		),
+	// Можно задать 2 состояния — steps и activeIndex
+	const [steps, setSteps] = useState(data); // я так и не поняла зачем создавать состояние для steps
+	const [activeIndex, setActiveIndex] = useState(0); // индекс данных каждого шага
+
+	// И определить 3 обработчика: Клик назад, Клик вперед, Начать сначала
+	const handleBackClick = () => {
+		setActiveIndex(activeIndex - 1);
+	};
+	const handleForwardClick = () => {
+		setActiveIndex(activeIndex + 1);
+	};
+	const handleRestartClick = () => {
+		setActiveIndex(0);
+	};
+	// И 2 переменных-флага — находимся ли мы на первом шаге, и находимся ли на последнем
+	const isTheFirstStep = activeIndex === 0;
+	const isTheLastStep = activeIndex === data.length - 1;
+
+	return (
+		<div className={styles.container}>
+			<div className={styles.card}>
+				<h1>Инструкция по готовке пельменей</h1>
+				<div className={styles.steps}>
+					<div className={styles['steps-content']}>
+						{steps[activeIndex].content}
+					</div>
+					<ul className={styles['steps-list']}>
+						{steps.map((step, index) => (
+							<li
+								key={step.id}
+								className={`${styles['steps-item']} ${index <= activeIndex ? styles.done : ''}`}
+							>
+								<button
+									onClick={() => setActiveIndex(index)}
+									className={styles['steps-item-button']}
+								>
+									{index + 1}
+								</button>
+								{step.title}
+							</li>
+						))}
+					</ul>
+					<button
+						onClick={() =>
+							isTheFirstStep ? handleRestartClick() : handleBackClick()
+						}
+						className={styles.button}
+						disabled={isTheFirstStep}
+					>
+						Назад
+					</button>
+					<button
+						onClick={() =>
+							isTheLastStep ? handleRestartClick() : handleForwardClick()
+						}
+						className={styles.button}
+					>
+						{isTheLastStep ? 'Начать сначала' : 'Далее'}
+					</button>
+				</div>
+			</div>
+		</div>
 	);
 };
